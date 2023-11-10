5070029bf2b586f2c45d14dc464f452c66092503
George Enesi <genesi@zojatech.com>
2023-11-10 21:48:03 +0100

    adding budget table

--- src/pages/budget-analysis.jsx
+++ src/pages/budget-analysis.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Budget = () => {
   return (
     <div>
@@ -8,6 +6,28 @@ const Budget = () => {
         <p>Set your budget according to your scale of preference</p>
 
         </div>
+        
+        <form action="">
+        <table className="table">
+          <th>
+            <tr>
+              <td>Item</td>
+              <td>Qty</td>
+              <td>Price</td>
+              <td>Subtotal</td>
+            </tr>
+            <tr>
+              <td><input type="text" name="" id="" /></td>
+              <td><input type="number" name="" id="" /></td>
+              <td><input type="number" name="" id="" /></td>
+              <td><input type="number" name="" id="" /></td>
+            </tr>
+          </th>
+        </table>
+          
+        </form>
+
+  
       
     </div>
   )
